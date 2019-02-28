31b89dcf055d6c3880c4b9be7a686f3bfc958633
Viacheslav Skakun <skavyacheslav@gmail.com>
2019-02-28 19:24:37 +0300

    Упражнение 5.Х

---
+++ random_letter1.py
@@ -0,0 +1,21 @@
+#Упражнение 5.1 (random_letter1)
+
+lst = ['самовар', 'весна', 'лето']
+
+import random
+lst1 = list(lst[random.randint(0, 2)])
+
+import copy
+lst2 = copy.deepcopy(lst1)
+
+n = random.randint(0, len(lst1))
+lst2[n] = '?'
+print(''.join(lst2))
+
+s = input('Введите букву: ')
+
+if s == lst1[n]:
+    print('\nПобеда!', '\nСлово: ' + ''.join(lst1))
+else:
+    print('\nУвы! Попробуйте в другой раз.', '\nСлово: ' + ''.join(lst1))
+
